77d6a897afa0799de29eb6ae424d8d88f9d61e17
carlsylvan <carlsylvan@gmail.com>
2022-12-06 08:40:04 +0100

    Product class model

---
+++ src/ts/main.ts
@@ -0,0 +1,4 @@
+import { Product } from "./models/Product";
+
+let product1 = new Product("Mjällschampo", 2, "ett schampo mot mjäll", 49);
+console.log(product1);
---
+++ src/ts/models/Product.ts
@@ -0,0 +1,4 @@
+export class Product {
+    constructor (public name: string, public category: number, public description: string, public cost: number) {
+    }
+}
