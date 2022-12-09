5bb869fabc18cef6ad77ca14ae2ef0d02b6cdbb8
Jesper <jesperhardin@gmail.com>
2022-12-09 11:28:16 +0100

    Added category, fixed margins

--- src/ts/models/categorylist.ts
+++ src/ts/models/categorylist.ts
@@ -5,9 +5,6 @@ export const categoryList: Category[] = [
     new Category(101, "Dagkräm"),
     new Category(102, "Serum"),
   ]),
-  new Category(2, "Kroppsvård", [
-    new Category(201, "Dusch"),
-    new Category(202, "Lotion"),
-    new Category(203, "hej"),
-  ]),
+  new Category(2, "Kroppsvård"),
+  new Category(3, "Solskydd"),
 ];
