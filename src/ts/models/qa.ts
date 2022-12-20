08d4e99daac6bcc4b87934470c07a5929c7e110d
carlsylvan <carlsylvan@gmail.com>
2022-12-20 11:41:55 +0100

    new faq class and question list

---
+++ src/ts/models/qa.ts
@@ -0,0 +1,9 @@
+export class QA {
+    constructor(
+        public question: string,
+        public answer: string
+    )
+    {}
+}
+
+export const faqList: QA[] = [new QA("Hur kommer jag i kontakt med er?", "Använd vårt formulär under 'kontakt'."), new QA("Var är mitt paket?", "När din order skickas från vårt lager får du en leveransbekräftelse till din e-postadress. I denna finns ett kolli-id/sändningsnummer som du kan klicka på för att spåra din försändelse."), new QA("Hur kan er sida vara så djävla bra?", "Hårt arbete")]
