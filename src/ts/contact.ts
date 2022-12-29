f8a76aab311a80d73a24c27a66a601e75bf49917
Jesper <jesperhardin@gmail.com>
2022-12-29 19:28:26 +0100

    fixed bug

--- src/ts/contact.ts
+++ src/ts/contact.ts
@@ -6,18 +6,18 @@ cartButton.addEventListener("click", toggleCart);
 closeButton.addEventListener("click", toggleCart);
 burger.addEventListener("click", burgerFunction);
 
-let submitbutton: HTMLButtonElement = document.getElementById("submitbutton") as HTMLButtonElement;
+let submitbutton: HTMLButtonElement = document.getElementById(
+  "submitbutton"
+) as HTMLButtonElement;
 let form: HTMLFormElement = document.getElementById("form") as HTMLFormElement;
-
 
 submitbutton.addEventListener("click", () => {
   form.reset();
-})
+});
 
 form.addEventListener("submit", (e: SubmitEvent) => {
   e.preventDefault();
   console.log("hej");
 });
-
 
 getFromLs();
